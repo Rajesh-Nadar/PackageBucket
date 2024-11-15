bc09e8fc59e806ffb626996965a0e4f24ba09b2d
Rajesh Nadar <51714639+Rajesh-Nadar@users.noreply.github.com>
2024-11-15 14:20:19 +0530

    Revert "Update SceneListWindow.cs"
    This reverts commit 89a30a5f6a7fc9e2db9cbcf6ed81a1fcdc3c4d16.

--- UnityTools/SceneList/Editor/SceneListWindow.cs
+++ UnityTools/SceneList/Editor/SceneListWindow.cs
@@ -4,178 +4,173 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneListWindow : EditorWindow
+namespace UnityTools
 {
-    private List<string> sceneNames = new List<string>();
-    private List<string> scenePaths = new List<string>();
-    private string searchQuery = "";
-    private Vector2 scroll;
-
-    private GUIContent refreshIcon;
-    private GUIContent saveIcon;
-    private GUIContent clearIcon;
-    private GUIContent restoreIcon;
-    private Texture2D sceneIcon;
-    private GUIContent addIcon;
-
-    [MenuItem("Tools/Scene List")]
-    static void Init()
+    public class SceneListWindow : EditorWindow
     {
-        SceneListWindow window = (SceneListWindow) EditorWindow.GetWindow(typeof(SceneListWindow));
-        window.titleContent = new GUIContent("Scene List");
-        window.Show();
-    }
+        private List<string> sceneNames = new List<string>();
+        private List<string> scenePaths = new List<string>();
+        private string searchQuery = "";
+        private Vector2 scroll;
 
-    private void OnEnable()
-    {
-        RefreshSceneList();
-        RestoreSearch();
+        private GUIContent refreshIcon;
+        private GUIContent saveIcon;
+        private GUIContent clearIcon;
+        private GUIContent restoreIcon;
+        private Texture2D sceneIcon;
+        private GUIContent addIcon;
 
-        refreshIcon = EditorGUIUtility.IconContent("d_RotateTool");
-        saveIcon = EditorGUIUtility.IconContent("SaveActive");
-        restoreIcon = EditorGUIUtility.IconContent("d_FolderOpened Icon");
-        clearIcon = EditorGUIUtility.IconContent("close");
-        addIcon = EditorGUIUtility.IconContent("d_CreateAddNew");
-        sceneIcon = EditorGUIUtility.IconContent("SceneAsset Icon").image as Texture2D;
-    }
+        [MenuItem("Unity Tools/Scene List")]
+        static void Init()
+        {
+            SceneListWindow window = (SceneListWindow)EditorWindow.GetWindow(typeof(SceneListWindow));
+            window.titleContent = new GUIContent("Scene List");
+            window.Show();
+        }
 
-    private void OnGUI()
-    {
-        EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
-        GUILayout.Label("Scene List", EditorStyles.boldLabel);
-
-        if (GUILayout.Button(refreshIcon, EditorStyles.toolbarButton))
+        private void OnEnable()
         {
             RefreshSceneList();
+            RestoreSearch();
+
+            refreshIcon = EditorGUIUtility.IconContent("d_RotateTool");
+            saveIcon = EditorGUIUtility.IconContent("SaveActive");
+            restoreIcon = EditorGUIUtility.IconContent("d_FolderOpened Icon");
+            clearIcon = EditorGUIUtility.IconContent("close");
+            addIcon = EditorGUIUtility.IconContent("d_CreateAddNew");
+            sceneIcon = EditorGUIUtility.IconContent("SceneAsset Icon").image as Texture2D;
         }
-        EditorGUILayout.EndHorizontal();
 
-        EditorGUILayout.BeginHorizontal();
-
-        searchQuery = EditorGUILayout.TextField("Search", searchQuery);
-
-        if (!string.IsNullOrEmpty(searchQuery))
+        private void OnGUI()
         {
-            if (GUILayout.Button(clearIcon, EditorStyles.toolbarButton, GUILayout.Width(30)))
+            EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
+            GUILayout.Label("Scene List", EditorStyles.boldLabel);
+
+            if (GUILayout.Button(refreshIcon, EditorStyles.toolbarButton))
             {
-                searchQuery = "";
+                RefreshSceneList();
             }
-            if (GUILayout.Button(saveIcon, EditorStyles.toolbarButton, GUILayout.Width(30)))
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+
+            searchQuery = EditorGUILayout.TextField("Search", searchQuery);
+
+            if (!string.IsNullOrEmpty(searchQuery))
             {
-                SaveSearch();
-            }
-        }
-        else
-        {
-            if (GUILayout.Button(restoreIcon, EditorStyles.toolbarButton, GUILayout.Width(30)))
-            {
-                RestoreSearch();
-            }
-        }
-        EditorGUILayout.EndHorizontal();
-
-        scroll = EditorGUILayout.BeginScrollView(scroll);
-
-        for (int i = 0; i < sceneNames.Count; i++)
-        {
-            string sceneName = sceneNames[i];
-
-            bool draw = false;
-
-            string[] query = searchQuery.ToLower().Split(",");
-
-            for (int q = 0; q < query.Length; q++)
-            {
-                string qr = query[q].Trim().ToLower();
-
-                if (!string.IsNullOrEmpty(qr))
+                if (GUILayout.Button(clearIcon, EditorStyles.toolbarButton, GUILayout.Width(30)))
                 {
-                    if (sceneName.ToLower().Contains(qr))
-                        draw = true;
+                    searchQuery = "";
+                }
+                if (GUILayout.Button(saveIcon, EditorStyles.toolbarButton, GUILayout.Width(30)))
+                {
+                    SaveSearch();
                 }
             }
-
-            if (searchQuery.Length == 0)
-                draw = true;
-
-            if (draw)
+            else
             {
-                EditorGUILayout.BeginHorizontal(GUI.skin.button);
-
-                if (GUILayout.Button(sceneIcon, GUI.skin.label, GUILayout.Width(18), GUILayout.Height(18)))
+                if (GUILayout.Button(restoreIcon, EditorStyles.toolbarButton, GUILayout.Width(30)))
                 {
-                    Object sceneAsset = AssetDatabase.LoadAssetAtPath<Object>(scenePaths[i]);
+                    RestoreSearch();
+                }
+            }
+            EditorGUILayout.EndHorizontal();
 
-                    if (sceneAsset != null)
+            scroll = EditorGUILayout.BeginScrollView(scroll);
+
+            for (int i = 0; i < sceneNames.Count; i++)
+            {
+                string sceneName = sceneNames[i];
+
+                bool draw = false;
+
+                string[] query = searchQuery.ToLower().Split(",");
+
+                for (int q = 0; q < query.Length; q++)
+                {
+                    string qr = query[q].Trim().ToLower();
+
+                    if (!string.IsNullOrEmpty(qr))
                     {
-                        EditorGUIUtility.PingObject(sceneAsset);
+                        if (sceneName.ToLower().Contains(qr))
+                            draw = true;
                     }
                 }
-                if (GUILayout.Button(sceneName, GUI.skin.label))
+
+                if (searchQuery.Length == 0)
+                    draw = true;
+
+                if (draw)
                 {
-                    if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                    EditorGUILayout.BeginHorizontal(GUI.skin.button);
+
+                    GUILayout.Label(sceneIcon, GUILayout.Width(18), GUILayout.Height(18));
+                    if (GUILayout.Button(sceneName, GUI.skin.label))
                     {
-                        EditorSceneManager.OpenScene(scenePaths[i]);
+                        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                        {
+                            EditorSceneManager.OpenScene(scenePaths[i]);
+                        }
                     }
-                }
-                if (GUILayout.Button(addIcon, GUI.skin.label, GUILayout.Width(20)))
-                {
-                    EditorSceneManager.SetActiveScene(EditorSceneManager.OpenScene(scenePaths[i], OpenSceneMode.Additive));
-                }
-                if (EditorSceneManager.sceneCount > 1)
-                {
-                    Scene scene = EditorSceneManager.GetSceneByPath(scenePaths[i]);
-                    Scene activeScene = default;
-
-                    int sceneCount = EditorSceneManager.loadedSceneCount;
-
-                    for (int k = 0; k < sceneCount; k++)
+                    if (GUILayout.Button(addIcon, GUI.skin.label, GUILayout.Width(20)))
                     {
-                        activeScene = EditorSceneManager.GetSceneAt(k);
+                        EditorSceneManager.SetActiveScene(EditorSceneManager.OpenScene(scenePaths[i], OpenSceneMode.Additive));
+                    }
+                    if (EditorSceneManager.sceneCount > 1)
+                    {
+                        Scene scene = EditorSceneManager.GetSceneByPath(scenePaths[i]);
+                        Scene activeScene = default;
+
+                        int sceneCount = EditorSceneManager.loadedSceneCount;
+
+                        for (int k = 0; k < sceneCount; k++)
+                        {
+                            activeScene = EditorSceneManager.GetSceneAt(k);
+
+                            if (scene == activeScene)
+                                break;
+                        }
 
                         if (scene == activeScene)
-                            break;
-                    }
-
-                    if (scene == activeScene)
-                    {
-                        if (GUILayout.Button(clearIcon, GUI.skin.label, GUILayout.Width(20)))
                         {
-                            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                            if (GUILayout.Button(clearIcon, GUI.skin.label, GUILayout.Width(20)))
                             {
-                                EditorSceneManager.CloseScene(scene, true);
+                                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                                {
+                                    EditorSceneManager.CloseScene(scene, true);
+                                }
                             }
                         }
                     }
+                    EditorGUILayout.EndHorizontal();
                 }
-                EditorGUILayout.EndHorizontal();
             }
+
+            EditorGUILayout.EndScrollView();
         }
 
-        EditorGUILayout.EndScrollView();
-    }
-
-    private void SaveSearch()
-    {
-        PlayerPrefs.SetString("sceneListSearchQuery", searchQuery);
-    }
-    private void RestoreSearch()
-    {
-        searchQuery = PlayerPrefs.GetString("sceneListSearchQuery", "");
-    }
-
-    private void RefreshSceneList()
-    {
-        sceneNames.Clear();
-        scenePaths.Clear();
-
-        int sceneCount = EditorSceneManager.sceneCountInBuildSettings;
-        for (int i = 0; i < sceneCount; i++)
+        private void SaveSearch()
         {
-            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
-            string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
-            sceneNames.Add(sceneName);
-            scenePaths.Add(scenePath);
+            PlayerPrefs.SetString("sceneListSearchQuery", searchQuery);
+        }
+        private void RestoreSearch()
+        {
+            searchQuery = PlayerPrefs.GetString("sceneListSearchQuery", "");
+        }
+
+        private void RefreshSceneList()
+        {
+            sceneNames.Clear();
+            scenePaths.Clear();
+
+            int sceneCount = EditorSceneManager.sceneCountInBuildSettings;
+            for (int i = 0; i < sceneCount; i++)
+            {
+                string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+                string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+                sceneNames.Add(sceneName);
+                scenePaths.Add(scenePath);
+            }
         }
     }
 }
